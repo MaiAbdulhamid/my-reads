5603083c05272b98c50d7e1ed59a2cafa88fe697
Mai Abdulhamid <mai.abdelhameed95@gmail.com>
2021-07-06 06:09:48 +0200

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -20,13 +20,13 @@ class Search extends Component {
       BooksAPI.search(query)
       .then(books => {
         if(books.error){
-          this.setState({ error: books })
+          this.setState({ error: books });
         }else {
-          this.setState({ result: books })
+          this.setState({ result: books });
         }
       })      
       .catch(error => {
-        this.setState({ result: error })
+        this.setState({ result: error });
       }) 
     }else {
       this.setState({ result: [] })
@@ -36,33 +36,30 @@ class Search extends Component {
     this.setState({query: query})
   }
   clearQuery = () => {
-    this.updateQuery('')
-  }
+    this.updateQuery('');
+  };
   handleSearchInput = (query) => {
-    this.updateQuery(query)
-    this.searchBooks(query)
-  }
+    this.updateQuery(query);
+    this.searchBooks(query);
+  };
 
   render(){
     const { query, result, error } = this.state;
     const { books, onChangeShelf } = this.props;
 
     const searchResult = result.map(book => {
-
       books.map(b => {
         if(b.id === book.id){
-          book.shelf = b.shelf
+          book.shelf = b.shelf;
         }
         return b;
       });
-
       return book;
-
     }).filter(b => (
       b.title.toLowerCase().includes(query.toLowerCase())
     ));
 
-    const searchTerms = ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS']
+    const searchTerms = ['Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'];
  
     return(
       <div className="search-books">
